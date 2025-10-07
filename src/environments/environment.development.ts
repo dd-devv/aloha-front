45bd05d856c1c24eac6f401f08a5f40b2c71f20b
Dino Roca <rocalaurented@gmail.com>
2025-10-06 21:47:00 -0500

    fix: api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,9 @@
 export const environment = {
-  // apiUrl: 'https://api.aloharestobar.com/api/',
-  // url_socket: 'https://api.aloharestobar.com',
-  apiUrl: 'http://localhost:3000/api/',
-  url_socket: 'http://localhost:3000',
+  apiUrl: 'https://api.aloharestobar.com/api/',
+  url_socket: 'https://api.aloharestobar.com',
+  // apiUrl: 'https://api2.aloharestobar.com/api/',
+  // url_socket: 'https://api2.aloharestobar.com',
+  // apiUrl: 'http://localhost:3000/api/',
+  // url_socket: 'http://localhost:3000',
   url_doc_sunat: 'https://back.apisunat.com/documents/',
 };
